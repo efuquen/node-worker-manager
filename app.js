d6b6162f7ce6bd3e210021ae300c68fa3ead99dc
Edwin Fuquen <edwin.fuquen@livestream.com>
2012-09-21 18:49:03 -0400

    updated with config initialize

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var inotify = require('inotify-plusplus').create(true)
 var fs = require('fs')
 var forever = require('forever-monitor')
+var _ = require('underscore')
 
 var runningChildren = {}
+var configDir = './worker-configs'
+
+function handleInotifyEvent(ev, filepathHandler) {
+  if(ev.hasOwnProperty("name") && ev.name.match("\\.json$")) {
+    var filename = ev.name
+    var dir = ev.watch
+    var filepath = dir + "/" + filename
+    filepathHandler(filepath)      
+  }
+}
 
 function startWorker(filepath) {
   fs.readFile(filepath, function(err, data) {
-    console.log("Read JSON: " + data)
     var child_config = JSON.parse(data.toString())
     var child = new (forever.Monitor)(child_config.filepath)
     runningChildren[filepath] = child
@@ -14,61 +24,35 @@ function startWorker(filepath) {
   })
 }
 
+function stopWorker(filepath) {
+  if(runningChildren.hasOwnProperty(filepath)) {
+    runningChildren[filepath].stop()
+    delete runningChildren[filepath]
+  } else {
+    console.log("Child DNE: " + filepath)
+  }
+}
+
+function restartWorker(filepath) {
+  if(runningChildren.hasOwnProperty(filepath)) {
+    runningChildren[filepath].restart()
+  } else {
+    console.log("Child DNE: " + filepath)
+  }
+}
+
+//initialize with what is already in the directory
+_.each(fs.readdirSync(configDir), function(filepath){ 
+  if(filepath.match("\\.json$")) {
+    startWorker(filepath) 
+  }
+})
+
 var directive = { 
-  create: function(ev) {
-    console.log("some things happened: " + JSON.stringify(ev));
-    if(ev.hasOwnProperty("name") && ev.name.match("\\.json$")) {
-      var filename = ev.name
-      var dir = ev.watch
-      var filepath = dir + "/" + filename
-      startWorker(filepath)      
-    }
-  },
-
-  delete: function(ev) {
-    console.log("some things happened: " + JSON.stringify(ev));
-    if(ev.hasOwnProperty("name") && ev.name.match("\\.json$")) {
-      var filename = ev.name
-      var dir = ev.watch
-      var filepath = dir + "/" + filename
-      if(runningChildren.hasOwnProperty(filepath)) {
-        runningChildren[filepath].stop()
-        delete runningChildren[filepath]
-      } else {
-        console.log("Child DNE: " + filepath)
-      }
-    }
-  },
-
-  attrib: function(ev) {
-    console.log("some things happened: " + JSON.stringify(ev));
-    if(ev.hasOwnProperty("name") && ev.name.match("\\.json$")) {
-      var filename = ev.name
-      var dir = ev.watch
-      var filepath = dir + "/" + filename
-      if(runningChildren.hasOwnProperty(filepath)) {
-        runningChildren[filepath].restart()
-      } else {
-        console.log("Child DNE: " + filepath)
-      }
-    }
-  },
-
-  modify: function(ev) {
-    console.log("some things happened: " + JSON.stringify(ev));
-    if(ev.hasOwnProperty("name") && ev.name.match("\\.json$")) {
-      var filename = ev.name
-      var dir = ev.watch
-      var filepath = dir + "/" + filename
-      if(runningChildren.hasOwnProperty(filepath)) {
-        runningChildren[filepath].restart()
-      } else {
-        console.log("Child DNE: " + filepath)
-      }
-    }
-  },
+  create: function(ev) { handleInotifyEvent(ev, startWorker) }
+  delete: function(ev) { handleInotifyEvent(ev, stopWorker) }
+  attrib: function(ev) { handleInotifyEvent(ev, restartWorker) }
+  modify: function(ev) { handleInotifyEvent(ev, restartWorker) }
 }
-var options =  {
- // all_events_is_catchall: true
-}
-inotify.watch(directive, './worker-configs', options);
+var options =  {}
+inotify.watch(directive, configDir, options);
